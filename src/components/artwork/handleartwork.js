bfc6a6d3b3b6a4eebdef3b2503cfb148cc9bd61c
Ina Yoon <itsina96@gmail.com>
2020-05-07 10:52:17 +0100

    Set Display function for props #Relates 13
    Co-authored-by: lizzy-j <lizzyhj@hotmail.com>

--- src/components/artwork/handleartwork.js
+++ src/components/artwork/handleartwork.js
@@ -1,21 +1,61 @@
-function handleArtwork() {
-  //   function genObjectId() {
-  let arr = [2558, 1, 2, 3, 4, 5, 6, 7, 8];
-  // for (let i = 0; i < arr.length; i++) {
-  //   return arr[i];
-  // }
-  //   }
+// function handleArtwork() {
+//   let arr = [438011, 435580, 436957, 436723, 437581];
+//   let index = Math.floor(Math.random() * arr.length);
+
+//   fetch(
+//     `https://collectionapi.metmuseum.org/public/collection/v1/objects/${arr[index]}`
+//   )
+//     .then((res) => res.json())
+//     .then((data) => console.log(data.primaryImage))
+//     .catch((err) => console.log(err));
+//   //Out of 474,000 objects we require 100 objects
+//   //Loop through the 100 objects and check to see if they have an image
+//   // If the object has an image then great
+// }
+
+// export default handleArtwork;
+
+const checkResponse = (response) => {
+  if (response.status !== 200) {
+    console.log(`Error with the request! ${response.status}`);
+    return;
+  }
+  return response.json();
+};
+
+const handleArtwork = () => {
+  // The reason why we fetch through an array of specific objectIDs is because there are more than 475,000 'objects' in total, and it significantly slows down (something). Also there were objects which don't have image property.
+  let arr = [
+    438011,
+    435580,
+    436957,
+    436723,
+    437581,
+    438011,
+    25656,
+    78141,
+    78060,
+    53219,
+    208222,
+    436532,
+    459112,
+    11234,
+    438815,
+    459106,
+    435875,
+    435885,
+    435683,
+    437052,
+  ];
   let index = Math.floor(Math.random() * arr.length);
 
-  fetch(
+  return fetch(
     `https://collectionapi.metmuseum.org/public/collection/v1/objects/${arr[index]}`
   )
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
-  //Out of 474,000 objects we require 100 objects
-  //Loop through the 100 objects and check to see if they have an image
-  // If the object has an image then great
-}
+    .then(checkResponse)
+    .catch((err) => {
+      throw new Error(`fetch getUserData failed ${err}`);
+    });
+};
 
 export default handleArtwork;
